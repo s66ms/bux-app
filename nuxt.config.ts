2e95f0026297dcac8b944b0bb0477d6f15526fdf
s66ms <seemszxc@gmail.com>
2025-10-07 11:31:21 +0200

    111

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  css: ['~/assets/scss/main.scss'],
+
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/ui',
+    "@nuxtjs/i18n"
+  ]
+})
