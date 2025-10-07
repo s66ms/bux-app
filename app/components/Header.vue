6204890bdb7dda91123d84994388b6cfd7693e7d
s66ms <seemszxc@gmail.com>
2025-10-07 11:31:15 +0200

    111

---
+++ app/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="header">
+    <div class="header__container _container">
+      <a href="" class="header__logo"><img src="../assets/img/logo.svg" alt=""></a>
+      <div class="header__menu menu">
+        <nav class="menu_body">
+          <ul class="menu_list">
+            <li><a href="" class="menu__link" style="color: #EE4312;">Home</a></li>
+            <li><a href="" class="menu__link">About</a></li>
+            <li><a href="" class="menu__link">Service</a></li>
+            <li><a href="" class="menu__link">Blog</a></li>
+            <li><a href="" class="menu__link">Contact</a></li>
+          </ul>
+        </nav>
+        </div>
+        <my-button>Book on Appoinment</my-button>
+    </div>
+  </header>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+@use '~/assets/scss/header';
+</style>
---
+++ app/components/UI/MyButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button :class="my-button">
+
+  </button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.my-button {
+  padding: 10px 20px;
+  border: 1px;
+  background-color: #EE4312;
+  width: 100%;
+  height: 50px;
+  font-size: 18px;
+}
+</style>
